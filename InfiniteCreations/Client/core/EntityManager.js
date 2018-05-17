b0e205b488e710e0329de25bf0f55fe434ecb7e8
LegitSoulja <support@legitsoulja.info>
2018-05-17 06:19:43 -0400

    fixed camera, entity manager, and skybox

---
+++ InfiniteCreations/Client/core/EntityManager.js
@@ -0,0 +1,30 @@
+﻿define([], function () {
+
+    return class EntityManager {
+
+        constructor(pc) {
+            this.pc = pc;
+        }
+
+        createEntity(name, entity, components, scripts) {
+            if(Object.hasOwnProperty(this.entites, name)) 
+                throw new Error("Cannot create entities with the same name");
+            var entity = new this.pc.Entity(entity);
+            entity.name = name;
+            if (components instanceof Object) {
+                for (var i in components) {
+                    entity.addComponent(i, components[i] || {})
+                }
+            }
+            if (scripts instanceof Object) {
+                if(!entity.script) entity.addComponent('script');
+                for (var i in scripts) {
+                    entity.script.create(i, scripts[i] || {});
+                }
+            }
+            return entity;
+        }
+
+    }
+
+})
